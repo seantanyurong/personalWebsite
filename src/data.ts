bdea47668689123cfc1d4d92d3964bebade95a5e
Sean Tan <seantan1218@gmail.com>
2022-12-22 17:52:18 +0800

    Added projects

---
+++ src/data.ts
@@ -0,0 +1,53 @@
+export const info = {
+  projects: [
+    {
+      title: "The 100 Club",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-1.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Olympus Origins",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-2.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Punchin",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-3.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "GMK ART",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-4.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Olympus Pages",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-5.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "One Olympus",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-6.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Kolectiv",
+      isFeatured: true,
+      thumbnail: "/assets/portfolio-7.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
